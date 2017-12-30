8ddb1ea6c750d2d34020d721f7cdb116407300a2
egordon9dev <egordon9dev@gmail.com>
2017-12-29 21:03:33 -0500

    holy moly this actually works..... Jump Point Search implemented. Search speed is 10 to 20 times faster!!!!!!

---
+++ A Star/build/classes/a/star/AStar.rs
@@ -0,0 +1,5 @@
+a.star.Node
+a.star.MyPanel
+a.star.AStar$1
+a.star.AStar
+a.star.NodeComparator
--- A Star/src/a/star/AStar.java
+++ A Star/src/a/star/AStar.java
@@ -10,11 +10,16 @@ import java.util.HashMap;
 import java.util.PriorityQueue;
 
 class MyPanel extends JPanel implements ActionListener {
-    int x;
-    int y;
-    int pos;
-    public MyPanel(LayoutManager layout) {
+    private int x, y, pos;
+    private AStar aStar;
+    private int gridWidth, gridHeight, nodeSpace;
+    public ArrayList<Node> path;
+    public MyPanel(LayoutManager layout, AStar aStar) {
         super(layout);
+        this.aStar = aStar;
+        gridWidth = aStar.getGridWidth();
+        gridHeight = aStar.getGridHeight();
+        nodeSpace = aStar.getNodeSpace();
         setFocusable(true);
         setBackground(Color.WHITE);
         setDoubleBuffered(true);
@@ -32,55 +37,58 @@ class MyPanel extends JPanel implements ActionListener {
         super.paintComponent(g);
         final int k = 25;
         g.setColor(new Color(160, 160, 160));
-        g.fillRect(k, k, AStar.GRID_WIDTH * AStar.NODE_SPACE, AStar.GRID_HEIGHT * AStar.NODE_SPACE);
+        g.fillRect(k, k, gridWidth * nodeSpace, gridHeight * nodeSpace);
         g.setColor(new Color(100, 100, 100, 150));
-        if (AStar.finished) {
-            ArrayList<Node> path = new ArrayList<Node>();
-            g.setColor(Color.WHITE);
-            for(int y = 0; y < AStar.nodes.length; y++) {
-                for(int x = 0; x < AStar.nodes[0].length; x++) {
-                    Node n = AStar.nodes[y][x];
-                    if(!n.walkable) {
-                        g.fillRect((int)n.x + k, (int)n.y + k, AStar.NODE_SPACE, AStar.NODE_SPACE);
-                    }
-                }
-            }
-            g.setColor(Color.BLACK);
-            Node n = AStar.end;
-            g.fillRect((int) n.x + k, (int) n.y + k, AStar.NODE_SPACE, AStar.NODE_SPACE);
-            path.add(n);
-            while (AStar.parent.containsKey(n)) {
-                n = AStar.parent.get(n);
-                g.fillRect((int) n.x + k, (int) n.y + k, AStar.NODE_SPACE, AStar.NODE_SPACE);
-                path.add(n);
-            }
-            if(pos == -1) {
-                pos = path.size()-1;
-            }
-            
-            double speed = 5;
-            g.setColor(new Color(150, 255, 150));
-            g.fillOval(x + k, y + k, AStar.NODE_SPACE, AStar.NODE_SPACE);
-            if(pos > 0) {
-                Node current = path.get(pos);
-                Node next = path.get(pos-1);
-                
-                double xv = (next.x - current.x);
-                double yv = (next.y - current.y);
-                double mag = Math.sqrt(Math.pow(xv, 2) + Math.pow(yv, 2));
-                xv *= speed/mag;
-                yv *= speed/mag;
-                x += xv;
-                y += yv;
-                
-                if(x >= next.x && y >= next.y) {
-                    pos--;
+        Node nodes[][] = aStar.getNodes();
+        g.setColor(Color.WHITE);
+        for(int y = 0; y < nodes.length; y++) {
+            for(int x = 0; x < nodes[0].length; x++) {
+                Node n = nodes[y][x];
+                if(!n.walkable) {
+                    g.fillRect((int)n.x + k, (int)n.y + k, nodeSpace, nodeSpace);
                 }
             }
         }
+        if(path != null) {
+            g.setColor(Color.BLACK);
+            for(int i = 0; i < path.size(); i++) {
+                Node n = path.get(i);
+                g.fillRect((int) n.x + k, (int) n.y + k, nodeSpace, nodeSpace);
+            }
+        }/*
+        if(dir != null) {
+            for(int i = 0; i < dir.size(); i++) {
+                int[] d = dir.get(i);
+                g.setFont(new Font("TimesRoman", Font.BOLD, 18));
+                g.drawString("cx: " + d[0] + "\tcy: " + d[1] + "\tdx: " + d[2] + "\tdy: " + d[3], 300, 50 + 30*i);
+            }
+        }*/
+            /*
+        if(pos == -1) {
+            pos = path.size()-1;
+        }
+        double speed = 5;
+        g.setColor(new Color(150, 255, 150));
+        g.fillOval(x + k, y + k, nodeSpace, nodeSpace);
+        if(pos > 0) {
+            Node current = path.get(pos);
+            Node next = path.get(pos-1);
+
+            double xv = (next.x - current.x);
+            double yv = (next.y - current.y);
+            double mag = Math.sqrt(Math.pow(xv, 2) + Math.pow(yv, 2));
+            xv *= speed/mag;
+            yv *= speed/mag;
+            x += xv;
+            y += yv;
+
+            if(x >= next.x && y >= next.y) {
+                pos--;
+            }
+        }*/
         g.setColor(Color.BLACK);
         g.setFont(new Font("TimesRoman", Font.BOLD, 30)); 
-        g.drawString(Long.toString(AStar.t1-AStar.t0), 200, 30);
+        g.drawString(Long.toString(aStar.getDt()), 200, 30);
         Toolkit.getDefaultToolkit().sync();
     }
 
@@ -117,10 +125,6 @@ class Node {
         walkable = b;
     }
 
-    public Rectangle toRect() {
-        return new Rectangle((int) x, (int) y, AStar.NODE_SPACE, AStar.NODE_SPACE);
-    }
-
     public boolean isCloseTo(double a, double b) {
         return Math.abs(a - b) < 0.001;
     }
@@ -133,19 +137,43 @@ class Node {
 }
 
 public class AStar {
-    private boolean finished = false;
-    public final int GRID_WIDTH = 200, GRID_HEIGHT = 200;
-    private MyPanel panel;
-    private JFrame frame;
-    private Node nodes[][] = new Node[GRID_HEIGHT][GRID_WIDTH];
-    
+    private static MyPanel panel;
+    private int gridWidth, gridHeight, nodeSpace;
+    private Node nodes[][];
     private Node start, end;
     private HashMap<Node, Node> parent;
-    public final int NODE_SPACE = 4;
+    
     public long t0, t1;
-    private void setupGUI() {
-        panel = new MyPanel(new FlowLayout());
-        frame = new JFrame();
+    public Node[][] getNodes() { return nodes; }
+    public int getGridWidth() { return gridWidth; }
+    public int getGridHeight() { return gridHeight; }
+    public int getNodeSpace() { return nodeSpace; }
+    public long getDt() { return t1 - t0; }
+    public AStar(int width, int height, int space) {
+        gridWidth = width;
+        gridHeight = height;
+        nodeSpace = space;
+        nodes = new Node[gridHeight][gridWidth];
+        for (int i = 0; i < nodes.length; i++) {
+            for (int j = 0; j < nodes[i].length; j++) {
+                nodes[i][j] = new Node();
+                nodes[i][j].x = j * nodeSpace;
+                nodes[i][j].y = i * nodeSpace;
+                
+                if((i == 150 && j > 5 && j <= 150) || (j == 150 && i >= 5 && i <= 150)) nodes[i][j].walkable = false; // ~ 105ms
+                /*
+                if(j == 40 && i <= 100) nodes[i][j].walkable = false;
+                if(i == 130 && j <= 100) nodes[i][j].walkable = false;
+                if(j == 100 && i > 5 && i <= 130) nodes[i][j].walkable = false;
+                if((i == 150 && j > 5 && j <= 150) || (j == 150 && i >= 0 && i <= 150)) nodes[i][j].walkable = false;*/ // ~ 75ms  --> 6ms
+
+            }
+        }
+        panel = new MyPanel(new FlowLayout(), this);
+    }
+    public static void setupGUI(ArrayList<Node> path) {
+        panel.path = path;
+        JFrame frame = new JFrame();
         frame.add(panel);
         frame.setSize(856, 878);// 56, 78
         frame.setResizable(false);
@@ -174,17 +202,30 @@ public class AStar {
             successors[i] = null;
         }
         Node n00 = nodes[0][0];
-        int cx = (int)(cur.x - n00.x) / NODE_SPACE, cy = (int)(cur.y - n00.y) / NODE_SPACE;
+        int cx = (int)(cur.x - n00.x) / nodeSpace, cy = (int)(cur.y - n00.y) / nodeSpace;
         if(parent.containsKey(cur)) {
             Node p = parent.get(cur);
-            int dx = cx - (int)(p.x - n00.x)/NODE_SPACE, dy = cy - (int)(p.y - n00.y)/NODE_SPACE;
+            int dx = cx - (int)(p.x - n00.x)/nodeSpace, dy = cy - (int)(p.y - n00.y)/nodeSpace;
             if(dx > 1) dx = 1;
             if(dx < -1) dx = -1;
             if(dy > 1) dy = 1;
             if(dy < -1) dy = -1;
             Node neighbors[] = getNeighborsPrune(cx, cy, dx, dy);
-            
             for(int i = 0; i < neighbors.length; i++) {
+                Node n = neighbors[i];
+                if(n == null) continue;
+                int nx = (int)(n.x-n00.x)/nodeSpace, ny = (int)(n.y-n00.y)/nodeSpace;
+            }
+            for(int i = 0; i < neighbors.length; i++) {
+                Node n = neighbors[i];
+                if(n == null) continue;
+                int nx = (int)(n.x-n00.x)/nodeSpace, ny = (int)(n.y-n00.y)/nodeSpace;
+                dx = nx - cx;
+                dy = ny - cy;
+                if(dx > 1) dx = 1;
+                if(dx < -1) dx = -1;
+                if(dy > 1) dy = 1;
+                if(dy < -1) dy = -1;
                 Node jumpPt = jump(cx, cy, dx, dy);
                 successors[i] = jumpPt;
             }
@@ -206,13 +247,13 @@ public class AStar {
     private Node jump(int cx, int cy, int dx, int dy) {
         int nx = cx + dx;
         int ny = cy + dy;
-        Node next = nodes[ny][nx];
         if(!isWalk(ny, nx)) return null;
+        Node next = nodes[ny][nx];
         if(next.equals(end)) return next;
         
         if(dx != 0 && dy != 0) { //diagonal
             //diagonal forced neighbor check
-            if((!isWalk(ny, nx-dx) && isWalk(ny+dy, nx) && isWalk(ny+dy, nx-dx)) || (!isWalk(ny, nx+dx) && isWalk(ny+dy, nx) && isWalk(ny+dy, nx+dx))) {
+            if((!isWalk(ny, nx-dx) && isWalk(ny+dy, nx) && isWalk(ny+dy, nx-dx)) || (!isWalk(ny-dy, nx) && isWalk(ny-dy, nx+dx) && isWalk(ny, nx+dx))) {
                 return next;
             }
             
@@ -227,11 +268,16 @@ public class AStar {
         }
         return jump(nx, ny, dx, dy);
     }
-    public Node[] getNeighborsPrune(int cx, int cy, int dx, int dy){
+    private Node[] getNeighborsPrune(int cx, int cy, int dx, int dy){
         Node[] neighbors = new Node[5];
+        for(int i = 0; i < neighbors.length; i++) {
+            neighbors[i] = null;
+        }
         if (dx!=0 && dy!=0) { // moving diagonal
             // normal 3 neighbors
-            if (isWalk(cy+dy, cx)) neighbors[0] = nodes[cy+dy][dx];
+            if (isWalk(cy+dy, cx)) {
+                neighbors[0] = nodes[cy+dy][cx];
+            }
             if (isWalk(cy, cx+dx)) neighbors[1] = nodes[cy][cx+dx];
             if ((isWalk(cy+dy, cx) || isWalk(cy, cx+dx)) && isWalk(cy+dy, cx+dx)) neighbors[2] = nodes[cy+dy][cx+dx];
             // 2 forced neighbors
@@ -244,7 +290,7 @@ public class AStar {
                     neighbors[0] = nodes[cy+dy][cx];
                     // 2 forced neighbors
                     if (!isWalk(cy, cx+1) && isWalk(cy+dy, cx+1)){
-                        neighbors[1] = nodes[cy+dy][dx+1];
+                        neighbors[1] = nodes[cy+dy][cx+1];
                     }
                     if (!isWalk(cy, cx-1) && isWalk(cy+dy, cx-1)){
                         neighbors[2] = nodes[cy+dy][cx-1];
@@ -276,27 +322,12 @@ public class AStar {
         }
         return path;
     }
-    public ArrayList<Node> findPath() {
-        setupGUI();
+    public ArrayList<Node> findPath(Node start, Node end) {
+        this.start = start;
+        this.end = end;
+        
         t0 = System.currentTimeMillis();
         t1 = Long.MAX_VALUE;
-        for (int i = 0; i < nodes.length; i++) {
-            for (int j = 0; j < nodes[i].length; j++) {
-                nodes[i][j] = new Node();
-                nodes[i][j].x = j * NODE_SPACE;
-                nodes[i][j].y = i * NODE_SPACE;
-                
-                if((i == 150 && j > 5 && j <= 150) || (j == 150 && i >= 5 && i <= 150)) nodes[i][j].walkable = false; // ~ 105ms
-                /*
-                if(j == 40 && i <= 100) nodes[i][j].walkable = false;
-                if(i == 130 && j <= 100) nodes[i][j].walkable = false;
-                if(j == 100 && i > 5 && i <= 130) nodes[i][j].walkable = false;
-                if((i == 150 && j > 5 && j <= 150) || (j == 150 && i >= 0 && i <= 150)) nodes[i][j].walkable = false; // ~ 75ms
-*/
-            }
-        }
-        start = nodes[0][0];
-        end = nodes[nodes.length - 1][nodes[0].length - 1];
         start.gScore = 0;
         start.fScore = dist(start, end);
         Comparator<Node> comparator = new NodeComparator();
@@ -311,14 +342,13 @@ public class AStar {
             Node current = openSet.remove();
             if (current == end) {
                 t1 = System.currentTimeMillis();
-                System.out.println("success");
-                finished = true;
                 return reconstructPath();
             }
             closedSet.put(current, null);
             successors = idSuccessors(current);
             for (int i = 0; i < successors.length; i++) {
                 Node n = successors[i];
+                if(n == null) continue;
                 if(closedSet.containsKey(n)) continue;
                 double tentative_gScore = current.gScore + dist(current, n);
                 //not a better path
@@ -331,11 +361,12 @@ public class AStar {
                 parent.put(n, current);
             }
         }
-        System.out.println("failure");
-        finished = true;
+        return null;
     }
-    public static void main() {
-        AStar aStar = new AStar();
-        aStar.findPath();
+    public static void main(String args[]) {
+        AStar aStar = new AStar(200, 200, 4);
+        Node nodes[][] = aStar.getNodes();
+        ArrayList<Node> path = aStar.findPath(nodes[0][0], nodes[nodes.length-1][nodes[0].length - 1]);
+        setupGUI(path);
     }
 }
